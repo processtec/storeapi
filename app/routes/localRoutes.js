626bf609d4a1e029ff8701a479a07215e6235bbb
Parul Mann <thedigipane@gmail.com>
2020-12-28 17:12:30 -0800

    no change just formatting

--- app/routes/localRoutes.js
+++ app/routes/localRoutes.js
@@ -4,43 +4,38 @@
 "use strict";
 
 /** controllers goes here */
-const edataComponents = require('../useCases/edata/logstashComponentsController');
-const localUser = require('../useCases/local/user/localUserController');
-const localComponent = require('../useCases/local/components/localComponentsController');
-const init = require('../useCases/local/initialization/dbInitializationController');
+const edataComponents = require("../useCases/edata/logstashComponentsController");
+const localUser = require("../useCases/local/user/localUserController");
+const localComponent = require("../useCases/local/components/localComponentsController");
+const init = require("../useCases/local/initialization/dbInitializationController");
 
 const register = (options) => {
-    const {
-        router
-    } = options;
+  const { router } = options;
 
-    // Doesn't requires JWT
+  // Doesn't requires JWT
 
-    // EDATA
-    router.post('/edata/components', edataComponents.addStocks);
+  // EDATA
+  router.post("/edata/components", edataComponents.addStocks);
 
-    // User Creation
-    router.post('/username', localUser.getUserByName);
-    router.post('/useremail', localUser.getUserByEmail);
-    router.post('/user', localUser.addUser);
+  // User Creation
+  router.post("/username", localUser.getUserByName);
+  router.post("/useremail", localUser.getUserByEmail);
+  router.post("/user", localUser.addUser);
 
-    router.put('/user', localUser.updatePassword);
-    router.put('/user_force', localUser.updatePasswordForce);
+  router.put("/user", localUser.updatePassword);
+  router.put("/user_force", localUser.updatePasswordForce);
 
+  // search
 
-    // search
+  // components
+  router.get("/components", localComponent.getComponents);
 
-    // components
-    router.get('/components', localComponent.getComponents);
+  // DB init
 
-
-    // DB init
-
-    // Components
-    router.post('/init/components', init.initComponents);
-
+  // Components - Not in  use anymore.
+  router.post("/init/components", init.initComponents);
 };
 
 module.exports = {
-    register
+  register,
 };
